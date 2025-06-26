eadf682aff95d610175e02d5a33bcfa6cf51156d
Warp10dot <warp100@protonmail.com>
2025-06-27 00:44:00 +0100

    Render.com deployment

--- Endless-Battler/server.js
+++ Endless-Battler/server.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(express.json());
 
-app.post('api/send', (req, res) => {
+app.post('https://ai-endless-battler.onrender.com/api/send', (req, res) => {
     const message = (req.body.message[0]);
     const defender = (req.body.message[1]);
     const log = (req.body.message[2] || '');
